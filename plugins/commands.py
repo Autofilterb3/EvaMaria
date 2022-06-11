d9250fd58ef3a6164088f637bab26e851c217623
Autofilterb3 <107288498+Autofilterb3@users.noreply.github.com>
2022-06-11 15:20:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,7 +44,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='@autofilterown_bot'),
             InlineKeyboardButton('🌀 MH LINKS 🌀', url='https://t.me/+gyZFP-mFh7YyN2Q1')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
@@ -90,7 +90,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='@autofilterown_bot '),
             InlineKeyboardButton('🔹ALL MOVIES 🔹', url='https://t.me/+gyZFP-mFh7YyN2Q1')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
